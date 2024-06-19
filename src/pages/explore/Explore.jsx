f3e1a00d901b4258b1c35438a1b7ed289502a7ee
= <pawan@bellatorgroup.in>
2024-06-19 12:31:37 +0530

    twin-rider

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import "./style.scss"
 
 const Explore = () => {
   return (
+
     <div style={{width:"100%"}}>
-      this is explore
-      lore
+
+ <section className="twin-rider-sec">
+  <div className="title">
+    <img src="" alt="" />
+    <img src="" alt="" />
+  </div>
+
+
+
+      </section>
     </div>
   )
 }
