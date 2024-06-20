915a97e3cf4022799b475fd238e7679a794d2e4d
= <pawan@bellatorgroup.in>
2024-06-20 16:21:30 +0530

    contact

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,4 +1,7 @@
 import headingImg from "./EEA.png"
 import twinRider from "./Twin-Riders.png"
 import sliderImg from "./ADULT-DOUBLE-01.png"
-export {headingImg, twinRider, sliderImg}
+import desertImg from "./desert_ex.jpg"
+import singleImg from "./Single-Riders.png"
+import adventureImg from "./Adventure-TRNS.png"
+export {headingImg, twinRider, sliderImg, desertImg, singleImg, adventureImg }
--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { headingImg ,twinRider} from '../../assets'
+import { adventureImg, desertImg, headingImg, singleImg, twinRider } from '../../assets'
 import "./styles.scss"
 import Rider from '../../component/slider/Rider'
 
@@ -18,12 +18,12 @@ const Explore = () => {
     centerPadding: "60px",
     slidesToShow: 1,
     speed: 500,
-    arrows:false
-    
+    arrows: false
+
   };
   return (
 
-    <div style={{width:"100%"}}>
+    <div style={{ width: "100%" }}>
 
 
 
@@ -37,33 +37,72 @@ const Explore = () => {
           <p>across stunning <span>
             sandy dunes.
           </span></p>
-       
+
         </div>
         <div className="slider-container">
-      <Slider {...settings}>
-          <SliderCard/>
-          <SliderCard/>
-          <SliderCard/>
-          <SliderCard/>
-        
-      </Slider>
-    </div>
+          <Slider {...settings}>
+            <SliderCard />
+            <SliderCard />
+            <SliderCard />
+            <SliderCard />
+
+          </Slider>
+        </div>
         {/* <Slider /> */}
       </section>
       <section className="twin-rider-sec">
-  <div className="title">
-    {/* <img src="" alt="" /> */}
-    <img src={twinRider} alt="twin-rider" />
-  </div>
-  <div className="ride-detail">
-    <h3> Adult (Double) </h3>
-    <p> Embrace Adventure: Thrilling Buggy Tours Await</p>
-  </div>
-  <div className="rider-slider">
+        <div className="title">
+          <img src={adventureImg} alt="twin-rider" />
+        </div>
+        <div className="title">
+      
+          <img src={twinRider} alt="twin-rider" />
+        </div>
+        <div className="ride-detail">
+          <h3> Adult (Double) </h3>
+          <p> Embrace Adventure: Thrilling Buggy Tours Await</p>
+        </div>
+        <div className="rider-slider">
+          <Rider />
 
-  <Rider />
-  </div>
-</section>
+        </div>
+      </section>
+
+
+      <section className="twin-rider-sec">
+        <div className="title banner">
+          <img src={adventureImg} alt="single-rider" />
+        </div>
+        <div className="title">
+          <img src={singleImg} alt="single-rider" />
+        </div>
+        <div className="ride-detail">
+          <h3> Adult (single) </h3>
+          <p> Embrace Adventure: Thrilling Buggy Tours Await</p>
+        </div>
+        <div className="rider-slider">
+
+          <Rider />
+        </div>
+      </section>
+
+      <section className="enquiry-section">
+        <div className="desert-img">
+          <img src={desertImg} alt="desert" />
+
+        </div>
+        <div className="thanks">
+          <h2>Thankyou</h2>
+          <div className="card-thank">
+            <p>We appreciate your interest in our services. One of our representatives 
+              will get back to you shortly with the information you requested. If 
+              you have any urgent questions, please feel free to contact us directly.</p>
+              <hr style={{width:"40%", marginBottom:"5px", marginTop:"10px"}} />
+              <p className="phone-number">+971 52 606 3261</p>
+          </div>
+        </div>
+
+      </section>
     </div>
   )
 }
