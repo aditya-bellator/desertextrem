73eddd82d21b64bb73e4af982fbc4b53d9b12c5a
Rajesh-dev3 <rajesh@bellatorgroup.in>
2024-06-20 11:16:54 +0530

    change

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import { headingImg } from '../../assets'
+
 ////styles
 import "./styles.scss"
+import Slider from 'react-slick';
 const Explore = () => {
+
+
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500,
+    
+  };
   return (
     <div style={{width:"100%"}}>
       <section className='experience-sec'>
@@ -15,6 +28,28 @@ const Explore = () => {
             sandy dunes.
           </span>
         </div>
+        <div className="slider-container">
+      <Slider {...settings}>
+        <div >
+          <h3>dafdsafsdfdsf1</h3>
+        </div>
+        <div>
+          <h3>asdasdfaf2</h3>
+        </div>
+        <div>
+          <h3>dsfsdfisfd</h3>
+        </div>
+        <div>
+          <h3>asdhbfdhbh</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
       </section>
     </div>
   )
