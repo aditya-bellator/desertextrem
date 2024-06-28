7fa5abd276310ab603ffcceca983a9555cd38d09
aditya-bellator <aditya@bellatorgroup.in>
2024-06-28 18:44:15 +0530

    new jsonm

--- src/component/json/Json.js
+++ src/component/json/Json.js
@@ -8,7 +8,7 @@ export const ExperienceSlides = [{
 }, {
     img: sharma180,
 
-    title: "<p> SHARMAX ATV CROSS 180 CC<br>2 seater<br>30 min : 150 AED<br>1 Hour : 300 AED</p>",
+    title: "<p> SHARMAX ATV 180 CC<br>2 seater<br>30 min : 150 AED<br>1 Hour : 300 AED</p>",
     id: "SHARMAX-ATV-CROSS-180-CC"
 
 },
@@ -67,7 +67,7 @@ export const ExperienceSlides = [{
 }, {
     img: sharmaatv250,
 
-    title: "<p> SHARMAX ATV 250 CROSS<br>2 seater<br>30 min : 150 AED<br>1 Hour : 300 AED</p>",
+    title: "<p> SHARMAX ATV 250<br>2 seater<br>30 min : 150 AED<br>1 Hour : 300 AED</p>",
     id: "005"
 }, {
     img: kymko250,
@@ -85,7 +85,23 @@ export const detailPageJson = [{
     name: "polaris rzr 1000 cc",
     description: "For all the racing enthusiast who like to show off their racing skills and drive like there is no tomorrow, come and join the fun of driving the powerful beast POLARIS RZR 1000 cc in Dubai as the ride takes you for an adventure of speed, thrill and power as you cruise your way through the dessert, away from the hustle and bustle of the city for an unforgettable experience. With its ease of use and simple guidelines, be ready to feel that adrenaline rush kick in as you speed through the wild landscape. ",
     inclusion: "<ul> <li>Pick & Drop service provided from the hotel </li><li> Driving Polaris RZR under expert supervision. </li> <li>Taking a moment to capture those amazing moments and memories in a photo. </li><li>Having a fun time with sandboarding and displaying those dessert skills. </li><li>Enjoy a refreshing and relaxing camel ride</li><li>Soft drinks served upon arrival and throughout the tour to keep that thirst quenched. </li><li>Helmets and sunglasses are provided. </li></ul> ",
-    Needtoknow: "<ul><li>To be eligible for refund/changes certain criteria on our tour need to be followed. </li><li>●	In case of unforeseen circumstances, and a refund is required, it must be documented to the booking manager through a booking channel like WhatsApp, email, or direct messaging.</li><li>	At least 24 hours before tour departure, in which case there will be a 100% refund being offered. But 24-12 hours, before departure, there will be a 50% refund and there will be no refund if the cancellation happens in less than 12 hours. </li><li>Tour departure is indicated in the booking confirmation. </li><li>It is important to consider that weather conditions do not influence term number</li></ul>",
+    Needtoknow: "<h3>Good to know </h3><p>To be eligible for refund/changes certain criteria on our tour need to be followed.</p><ul><li>●	In case of unforeseen circumstances, and a refund is required, it must be documented to the booking manager through a booking channel like WhatsApp, email, or direct messaging.</li><li>	At least 24 hours before tour departure, in which case there will be a 100% refund being offered. But 24-12 hours, before departure, there will be a 50% refund and there will be no refund if the cancellation happens in less than 12 hours. </li><li>Tour departure is indicated in the booking confirmation. </li><li>It is important to consider that weather conditions do not influence term number</li><p></p></ul>",
+    cancellationPollicy:cancellationPollicy,
+},
+{
+    id: "CANAM-RZR-1000CC-2-Seater",
+    name: "CANAM RZR 1000CC (2 Seater)",
+    description: "Let the adventure enthusiast in you satiate his/her quest for adventure with the thrill of speed and a chance to ride a CANAM RZR 1000CC and let the rush of speed, give you that necessary dopamine kick as you step away from the hustle of the city to step into the winds of the desert and take a moment to rejoice with the excitement of driving a really fast 1000CC Canam RZR in the desert for that once-in-a-lifetime moment.",
+    inclusion: "<p>Desert Xtreme is all set to give you an adrenaline rush, as you start on a fast and furious journey on a 1000 CC buggy through the desert and catch a glimpse of what speed, thrill and adventure feel like for a day.</p><ul> <li>The trip duration is 4 hours</li><li>You will get to ride the buggy for 120 minutes</li> <li>Pickup time is 12 pm</li><li>Having a fun time with sandboarding and displaying those dessert skills. </li><li>Drop off time is 4pm</li><li>Assisted riding </li><li>Stop and get clicked </li><li>You will get to ride a camel </li><li>●	You will get to sandboard </li><li>Get clicked with a falcon</li><li>	Helmet and sunglasses for the ride</li><li>Non-Alcoholic Beverages for refreshment</li></ul> ",
+    Needtoknow: "<p>The following steps make you eligible for a refund. Please read below for details about cancellation and refund process: </p><ul><li>Request for refund or any changes must be done in writing to the operation manager via Whatsapp or Instagram Direct Messages. </li><li>If you cancel your trip before 24 hours, the complete amount will be refunded</li><li>In case you cancel within 24-12 hours of your travel date then 50 % of the amount will be refunded</li><li>●	If you cancel the trip less than 12 hours before the tour date, no refund will be made to you. </li></ul><h3>Meta-description: </h3><p>Rejoice with the excitement of driving a really fast CANAM RZR 1000CC in the desert for that once-in-a-lifetime moment. Book to get best deals. </p>",
+    cancellationPollicy:cancellationPollicy,
+},
+{
+    id: "KYMKO MXU 250 CC ",
+    name: "KYMKO-MXU-250-CC",
+    description: "When it comes to the prowess of Kymku MXU 250 CC, then even Dubais dessert sandbanks fall short compared to the raw and vigorous power of the automatic ATV which takes you on an escalated thrill, as you take this machine for a spin. Dessert Safari provides you with an opportunity to let you drive this beautiful machine on the sand, all by yourself for 1 hour which will be monitored under the supervision of experts, just to ensure your safety. .",
+    inclusion: "<ul> <li>Pick & Drop service provided from the hotel </li><li>	Bike riding session under the supervision of our experts </li> <li>Relaxing time for a fun photo session, to capture all the memories. </li><li>Enjoy skiing on the sand for a fun ride of sandboarding.  </li><li>Explore the sand on the back of a camel as it takes a stroll for a camel ride. </li><li>Assisted riding </li><li>Refreshments and soft drinks are provided to customers. </li><li>Helmets and sunglasses are provided.  </li></ul> ",
+    Needtoknow: "<p>To be eligible for refund/changes certain criteria on our tour need to be followed.  </p><ul><li>●	In case of unforeseen circumstances, and a refund is required, it must be documented to the booking manager through a booking channel like WhatsApp, email, or direct messaging. </li><li>●	At least 24 hours before tour departure, in which case there will be a 100% refund being offered. But 24-12 hours, before departure, there will be a 50% refund and there will be no refund if the cancellation happens in less than 12 hours. </li><li>Tour departure is indicated in the booking confirmation. </li></ul><p>It is important to consider that weather conditions do not influence term number 2. </p>",
     cancellationPollicy:cancellationPollicy,
 }]
 export const Adultdouble = [{
