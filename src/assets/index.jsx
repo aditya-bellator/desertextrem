3a36aea580118305e5e3c492e220d4358725fee3
aditya-bellator <aditya@bellatorgroup.in>
2024-07-01 12:14:32 +0530

    slid img

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -37,5 +37,54 @@ import tickIcon from "./Tick-icon.png"
 import cardImage from "./POLARIS-RZR-1000-CC.png"
 
 
+// --------------------------------------------------------------
+import Canam1 from "./slid/canam1000/cana1.webp"
+import Canam2 from "./slid/CANAM1000/cana2.webp"
+import Canam3 from "./slid/canam1000/cana3.jpg"
+import Canam4 from "./slid/CANAM1000/cana4.webp"
+import Canam5 from "./slid/CANAM1000/cana5.jpg"
+import dart1 from "./slid/Dirt_bike_280/dart1.jpg"
+import dart2 from "./slid/Dirt_bike_280/dart2.jpg"
+import dart3 from "./slid/Dirt_bike_280/dart3.jpg"
+import dart4 from "./slid/Dirt_bike_280/dart4.webp"
+import dart5 from "./slid/Dirt_bike_280/dart5.webp"
+import kymko1 from "./slid/KYMKO_MXU/kymko1.jpg"
+import kymko2 from "./slid/KYMKO_MXU/kymko2.jpg"
+import kymko3 from "./slid/KYMKO_MXU/kymko3.png"
+import kymko4 from "./slid/KYMKO_MXU/kymko4.webp"
+import kymko5 from "./slid/KYMKO_MXU/kymko5.png"
+import pol1 from "./slid/POLARIS-4/pol1.webp"
+import pol2 from "./slid/POLARIS-4/pol2.jpeg"
+import pol3 from "./slid/POLARIS-4/pol3.jpg"
+import pol4 from "./slid/POLARIS-4/pol4.jpg"
+import pol5 from "./slid/POLARIS-4/pol5.jpg"
+import pol6 from "./slid/POLARIS-4/pol6.jpg"
+import pole1 from "./slid/Polaris-2/pole1.jpg"
+import pole2 from "./slid/Polaris-2/pole2.jpg"
+import pole3 from "./slid/Polaris-2/pole3.jpg"
+import pole4 from "./slid/Polaris-2/pole4.jpg"
+import pole5 from "./slid/Polaris-2/pole5.jpg"
+import yam1 from "./slid/Yamaha450-CC/yam1.png"
+import yam2 from "./slid/Yamaha450-CC/yam2.jpeg"
+import yam3 from "./slid/Yamaha450-CC/yam3.jpg"
+import yam4 from "./slid/Yamaha450-CC/yam4.webp"
+import yrapto1 from "./slid/YAMAHA-700-CC/yrapto1.jpg"
+import yrapto2 from "./slid/YAMAHA-700-CC/yrapto2.jpg"
+import yrapto3 from "./slid/YAMAHA-700-CC/yrapto3.jpg"
+import yrapto4 from "./slid/YAMAHA-700-CC/yrapto4.jpg"
+import yrapto5 from "./slid/YAMAHA-700-cc/yrapto5.jpg"
+import shar1 from "./slid/SHARMAX/shar1.webp"
+import shar2 from "./slid/SHARMAX/shar2.jpg"
+import shar3 from "./slid/SHARMAX/shar3.jpg"
+import shar4 from "./slid/SHARMAX/shar4.webp"
 
-export {kymko250,cardImage,favorite,favorite1,favorite2, desert,desert1,desert2,desert3,desert4,desert5,tickIcon,headingImg,last,sharma320,user, road1,half ,great ,bike, road2, cannam1000, cannam4,sharma180, bluebuggy ,polaris1000,sharmaATV250,sharmaatv250 ,twinRider, sliderImg, desertImg, singleImg, adventureImg, desertEx,logo, galleryImg,whiteBg, }
+
+
+
+
+
+
+
+
+
+export {shar1,shar2,shar3,shar4,yrapto1,yrapto2,yrapto3,yrapto4,yrapto5,yam1,yam2,yam3,yam4,pole1, pole2,pole3,pole4,pole5,pol1, pol2,pol3,pol4,pol5,pol6,kymko1,kymko2,kymko3,kymko4,kymko5,dart1,dart2,dart3,dart4, dart5,Canam1,Canam2,Canam3,Canam4,Canam5,kymko250,cardImage,favorite,favorite1,favorite2, desert,desert1,desert2,desert3,desert4,desert5,tickIcon,headingImg,last,sharma320,user, road1,half ,great ,bike, road2, cannam1000, cannam4,sharma180, bluebuggy ,polaris1000,sharmaATV250,sharmaatv250 ,twinRider, sliderImg, desertImg, singleImg, adventureImg, desertEx,logo, galleryImg,whiteBg, }
